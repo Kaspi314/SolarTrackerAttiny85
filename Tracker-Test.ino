4a2de7766ed626dee772d1d91ee444f4bae1ead8
Kaspi314 <71607355+Kaspi314@users.noreply.github.com>
2021-01-29 04:24:53 -0500

    Update Tracker-Test.ino
    added statistics methods.

--- Tracker-Test.ino
+++ Tracker-Test.ino
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 
 // Delay milliseconds
@@ -197,4 +196,71 @@ uint8_t get_motor_channel(struct Motor motor, uint8_t rotation)
     return motor.CW;
   }
   return motor.CCW;
+}
+
+
+
+void swap(int *x, int *y) {
+  if (x == y)
+    return;
+  *x ^= *y;
+  *y ^= *x;
+  *x ^= *y;
+}
+
+void cocktailsort(int *a, size_t n) {
+
+  while (1) {
+    // packing two similar loops into one
+    char flag;
+    size_t start[2] = {1, n - 1},
+                      end[2] = {n, 0},
+                               inc[2] = {1, -1};
+    for (int it = 0; it < 2; ++it) {
+      flag = 1;
+      for (int i = start[it]; i != end[it]; i += inc[it])
+        if (a[i - 1] > a[i]) {
+          swap(a + i - 1, a + i);
+          flag = 0;
+        }
+      if (flag)
+        return;
+    }
+  }
+}
+
+double mean(uint8_t* values, uint8_t n)
+{
+  cocktailsort(*values, n);
+  if (!(n % 2)) {
+    return (double)values[n / 2];
+  }
+  return (double)((values[n - 1 / 2] + values[(n - 1 / 2) + 1]) / 2);
+}
+
+size_t sizeof_t(uint8_t* values)
+{
+  return sizeof(values) / sizeof(values[0]);
+}
+
+double avg(uint8_t* values, uint8_t n)
+{
+  uint8_t i;
+  double s = 0;
+
+  for ( i = 0; i < n; i++ )
+    s += values[i];
+  return s / n;
+}
+
+
+double stddev(uint8_t* values, uint8_t n)
+{
+  uint8_t i;
+  double average = avg(values, n);
+  double s = 0;
+
+  for ( i = 0; i < n; i++ )
+    s += (values[i] - average) * (values[i] - average);
+  return sqrt(s / (n - 1));
 }
